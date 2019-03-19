71546ebb587eec890babd9f80d436ec4b6d8f9f3
nanbokku <n1510126@edu.cc.uec.ac.jp>
2019-03-19 20:41:29 +0900

    [add] Add codeigniter files.

---
+++ codeigniter/application/views/errors/cli/error_404.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+defined('BASEPATH') or exit('No direct script access allowed');
+
+echo "\nERROR: ",
+    $heading,
+    "\n\n",
+    $message,
+    "\n\n";
---
+++ codeigniter/application/views/errors/cli/error_db.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+defined('BASEPATH') or exit('No direct script access allowed');
+
+echo "\nDatabase error: ",
+    $heading,
+    "\n\n",
+    $message,
+    "\n\n";
